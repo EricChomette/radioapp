f9bfade6a7265d229f2480db1ba64a05a4fba33d
EricChomette <eric.chomette@gmail.com>
2022-01-31 15:34:38 +0100

    save

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,19 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "stylesheets/application";
+
+import { countDown } from "/Users/ericchomette/code/Projet/radioV1/radioapp/app/javascript/components/countdown.js"
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+document.addEventListener('turbolinks:load', () => {
+  countDown();
+});
+
+import "stylesheets/application";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
